fe88e0598b64d66502111af2e53937917a1c902d
Vaone <gvin8888@gmail.com>
2024-06-24 00:49:26 +0500

    feat: - server

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 
 const app = express()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
